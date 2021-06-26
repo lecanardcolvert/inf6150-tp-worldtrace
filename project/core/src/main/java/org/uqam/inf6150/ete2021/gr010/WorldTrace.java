8c4f0fdb59f974e555114711c8f68dc9df4f3f40
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-26 19:00:39 -0400

    Reformat

--- project/core/src/main/java/org/uqam/inf6150/ete2021/gr010/WorldTrace.java
+++ project/core/src/main/java/org/uqam/inf6150/ete2021/gr010/WorldTrace.java
@@ -9,27 +9,29 @@ import com.badlogic.gdx.utils.ScreenUtils;
  * Test JavaDoc generation.
  * TODO : remove JavaDoc test.
  */
-public class WorldTrace extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class WorldTrace
+        extends ApplicationAdapter {
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture     img;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img   = new Texture("badlogic.jpg");
+    }
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
--- project/desktop/src/main/java/org/uqam/inf6150/ete2021/gr010/desktop/DesktopLauncher.java
+++ project/desktop/src/main/java/org/uqam/inf6150/ete2021/gr010/desktop/DesktopLauncher.java
@@ -5,8 +5,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.uqam.inf6150.ete2021.gr010.WorldTrace;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new WorldTrace(), config);
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new WorldTrace(), config);
+    }
 }
