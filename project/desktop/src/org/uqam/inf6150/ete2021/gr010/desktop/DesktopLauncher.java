8364805eef74d21b0d5f2d7f7f90033e9d79f679
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-26 16:35:37 -0400

    Added the `core` and `desktop` projects' main Java classes

---
+++ project/desktop/src/org/uqam/inf6150/ete2021/gr010/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.uqam.inf6150.ete2021.gr010.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.uqam.inf6150.ete2021.gr010.WorldTrace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WorldTrace(), config);
+	}
+}
