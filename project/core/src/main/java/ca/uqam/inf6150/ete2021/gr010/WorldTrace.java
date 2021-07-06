9eadc9ca71cf746de8ab4f0a4dcc8542d8fa9c65
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-06 14:44:46 -0400

    Encapsulated member vars

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/WorldTrace.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/WorldTrace.java
@@ -12,26 +12,27 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class WorldTrace
         extends ApplicationAdapter {
 
-    SpriteBatch batch;
-    Texture     img;
+    private SpriteBatch m_batch;
+    private Texture     m_img;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img   = new Texture("assets/badlogic.jpg");
+        m_batch = new SpriteBatch();
+        m_img   = new Texture("assets/badlogic.jpg");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        
+        m_batch.begin();
+        m_batch.draw(m_img, 0, 0);
+        m_batch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
+        m_batch.dispose();
+        m_img.dispose();
     }
 }
