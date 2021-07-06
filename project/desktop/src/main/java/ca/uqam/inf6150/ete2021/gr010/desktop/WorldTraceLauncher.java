ff5bc97939e2c3c78526a34996b97ac04d544191
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-06 15:10:51 -0400

    Made the window not resizable

--- project/desktop/src/main/java/ca/uqam/inf6150/ete2021/gr010/desktop/WorldTraceLauncher.java
+++ project/desktop/src/main/java/ca/uqam/inf6150/ete2021/gr010/desktop/WorldTraceLauncher.java
@@ -14,6 +14,7 @@ public class WorldTraceLauncher {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
         config.setWindowedMode(1280, 720);
+        config.setResizable(false);
         config.useOpenGL3(true, 4, 2);
         config.useVsync(true);
         config.setIdleFPS(15);
