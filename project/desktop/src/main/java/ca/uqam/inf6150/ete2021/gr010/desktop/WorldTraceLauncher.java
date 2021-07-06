d9a8579d3013987488f864660166253d3fc18401
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-06 14:44:04 -0400

    Renamed file and added VSync

--- project/desktop/src/main/java/ca/uqam/inf6150/ete2021/gr010/desktop/WorldTraceLauncher.java
+++ project/desktop/src/main/java/ca/uqam/inf6150/ete2021/gr010/desktop/WorldTraceLauncher.java
@@ -8,10 +8,14 @@ import ca.uqam.inf6150.ete2021.gr010.WorldTrace;
  * Test JavaDoc generation.
  * TODO : remove JavaDoc test.
  */
-public class DesktopLauncher {
+public class WorldTraceLauncher {
 
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+        config.useVsync(true);
+        config.setIdleFPS(15);
+
         new Lwjgl3Application(new WorldTrace(), config);
     }
 }
