dc5019413befb0a070c44ef7e4b6c3e909549668
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-18 22:05:46 -0400

    Added buildServiceRegistry func

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/manualtest/DBConnectivity.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/manualtest/DBConnectivity.java
@@ -1,6 +1,7 @@
 package ca.uqam.inf6150.ete2021.gr010.flight.manualtest;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -10,15 +11,26 @@ public class DBConnectivity {
     private static final String HIBERNATE_CONFIG_FILE_PATH   = "hibernate.cfg.xml";
     private static final String ANNOTATED_BEANS_PACKAGE_NAME = "ca.uqam.inf6150.ete2021.gr010.flight";
 
-    public static SessionFactory getSessionFactory() {
-        StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+    public static SessionFactory buildSessionFactory() {
+        StandardServiceRegistry serviceRegistry = buildServiceRegistry();
+        Metadata                metadata        = buildMetadata(serviceRegistry);
 
-        StandardServiceRegistry serviceRegistry = serviceRegistryBuilder
-                .configure(HIBERNATE_CONFIG_FILE_PATH)
-                .build();
+        return metadata.buildSessionFactory();
+    }
 
+    private static Metadata buildMetadata(final StandardServiceRegistry serviceRegistry) {
         MetadataSources metadataSources = new MetadataSources(serviceRegistry);
         metadataSources.addPackage(getAnnotatedBeansPackage());
+
+        return metadataSources.buildMetadata();
+    }
+
+    private static StandardServiceRegistry buildServiceRegistry() {
+        StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+
+        return serviceRegistryBuilder
+                .configure(HIBERNATE_CONFIG_FILE_PATH)
+                .build();
     }
 
     private static Package getAnnotatedBeansPackage() {
