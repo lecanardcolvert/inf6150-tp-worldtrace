1c2b1f9c5a7b63cd4ce729393138bff0ee92622f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-18 22:58:13 -0400

    Modified Main for Spring Boot instead of Hibernate

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/manualtest/Main.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/manualtest/Main.java
@@ -1,50 +1,12 @@
 package ca.uqam.inf6150.ete2021.gr010.flight.manualtest;
 
-import javax.persistence.metamodel.EntityType;
-import org.hibernate.HibernateException;
-import org.hibernate.Metamodel;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
-    private static final SessionFactory ourSessionFactory;
-
-    static {
-        try {
-            Configuration configuration = new Configuration();
-            configuration.configure();
-
-            ourSessionFactory = configuration.buildSessionFactory();
-        }
-        catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static Session getSession() throws HibernateException {
-    //    return ourSessionFactory.openSession();
-        return DBConnectivity.getOrBuildSessionFactory().openSession();
-    }
-
-    public static void main(final String[] args) {
-
-        try (Session session = getSession()) {
-            System.out.println("querying all the managed entities...");
-            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-
-            for (EntityType<?> entityType : metamodel.getEntities()) {
-                final String   entityName = entityType.getName();
-                final Query<?> query      = session.createQuery("from " + entityName);
-
-                System.out.println("executing: " + query.getQueryString());
-
-                for (Object o : query.list()) {
-                    System.out.println("  " + o);
-                }
-            }
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
