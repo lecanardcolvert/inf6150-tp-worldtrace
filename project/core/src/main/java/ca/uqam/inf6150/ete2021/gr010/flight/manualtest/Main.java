a6a2edcdcd61974a51f73460b32f68a4c262f8da
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-18 22:03:26 -0400

    Hibernate generated Main class

---
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/manualtest/Main.java
@@ -0,0 +1,49 @@
+package ca.uqam.inf6150.ete2021.gr010.flight.manualtest;
+
+import jakarta.persistence.metamodel.EntityType;
+import org.hibernate.HibernateException;
+import org.hibernate.Metamodel;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class Main {
+
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) {
+
+        try (Session session = getSession()) {
+            System.out.println("querying all the managed entities...");
+            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
+
+            for (EntityType<?> entityType : metamodel.getEntities()) {
+                final String   entityName = entityType.getName();
+                final Query<?> query      = session.createQuery("from " + entityName);
+
+                System.out.println("executing: " + query.getQueryString());
+
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        }
+    }
+}
