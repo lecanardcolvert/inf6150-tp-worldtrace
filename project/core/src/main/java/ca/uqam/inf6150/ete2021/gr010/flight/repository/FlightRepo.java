1a231437f883e0b59c62e0927507963480267d23
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-19 13:31:41 -0400

    Use JPA repository

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/repository/FlightRepo.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/repository/FlightRepo.java
@@ -1,10 +1,12 @@
 package ca.uqam.inf6150.ete2021.gr010.flight.repository;
 
 import ca.uqam.inf6150.ete2021.gr010.flight.model.Flight;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlightRepo
-        extends CrudRepository<Flight, Long> {
+        extends JpaRepository<Flight, Long> {
 
     Flight findById(long p_id);
 }
