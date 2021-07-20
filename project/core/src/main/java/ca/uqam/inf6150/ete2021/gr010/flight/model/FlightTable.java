6b1b1820cea41e3afd03e420418b42e179d2a3fd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-19 21:47:47 -0400

    Added FlightTable

---
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/model/FlightTable.java
@@ -0,0 +1,13 @@
+package ca.uqam.inf6150.ete2021.gr010.flight.model;
+
+public interface FlightTable {
+
+    String TABLE_NAME             = "FLIGHT";
+    String COL_NAME_ID            = "AIRLINENO";
+    String COL_NAME_BEGIN_AIRPORT = "AIRPORTDEPARTURENO";
+    String COL_NAME_END_AIRPORT   = "AIRPORTARRIVALNO";
+    String COL_NAME_AIRCRAFT      = "AIRCRAFTNO";
+    String COL_NAME_AIRLINE       = "AIRLINENO";
+    String COL_NAME_DEPARTURE     = "DEPARTURE";
+    String COL_NAME_ARRIVAL       = "ARRIVAL";
+}
