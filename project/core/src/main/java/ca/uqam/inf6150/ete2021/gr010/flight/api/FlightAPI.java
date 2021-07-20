290ce8cc95f47a5e2d5eed7dc33fc8481f179a50
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-19 21:50:48 -0400

    Refactored FlightAPI

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/api/FlightAPI.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/api/FlightAPI.java
@@ -2,21 +2,27 @@ package ca.uqam.inf6150.ete2021.gr010.flight.api;
 
 import ca.uqam.inf6150.ete2021.gr010.flight.db.DBConnection;
 import ca.uqam.inf6150.ete2021.gr010.flight.model.Flight;
+import ca.uqam.inf6150.ete2021.gr010.flight.model.meta.FlightTable;
+import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
 
 public interface FlightAPI {
 
-    private static Class<Flight> getEntityClass() {
-        return Flight.class;
-    }
-
     static Flight getLatest() throws SQLException {
-        var dao = DBConnection.getOrCreate().getDao(getEntityClass());
+        var dao = getDao();
 
         return dao.queryBuilder()
-                  .orderByNullsLast("DEPARTURE", false)
+                  .orderByNullsLast(FlightTable.COL_NAME_DEPARTURE, false)
                   .limit(1L)
                   .queryForFirst();
+    }
+
+    static Dao<Flight, Long> getDao() throws SQLException {
+        return DBConnection.getOrCreate().getDao(getEntityClass());
+    }
+
+    private static Class<Flight> getEntityClass() {
+        return Flight.class;
     }
 }
