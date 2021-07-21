1c89f92504d972a438df788ad1fdb748ea0311c8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-21 18:51:42 -0400

    Revert "Merge branch 'feature/3-draw-plane-flight' of gitlab.info.uqam.ca:bensakhria.rebei_belkacem/tp-inf6150 into feature/3-draw-plane-flight"
    This reverts commit f26dbc81d414aca120d150eeebb51145d140c34b.

---
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/flight/model/Aircraft.java
@@ -0,0 +1,26 @@
+package ca.uqam.inf6150.ete2021.gr010.flight.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DatabaseTable(tableName = "AIRCRAFT")
+public final class Aircraft
+        implements Serializable {
+
+    @DatabaseField(columnName = "AIRCRAFTNO", generatedId = true, canBeNull = false)
+    private long m_id;
+
+    @DatabaseField(columnName = "MANUFACTURERNO", foreign = true, foreignAutoCreate = true, foreignAutoRefresh = true, maxForeignAutoRefreshLevel = 4, canBeNull = false)
+    private Manufacturer m_manufacturer;
+
+    @DatabaseField(columnName = "MODEL", canBeNull = false)
+    private String m_model;
+}
