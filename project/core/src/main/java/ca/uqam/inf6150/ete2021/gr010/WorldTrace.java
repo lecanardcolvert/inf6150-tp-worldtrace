6f22912c9a202bd8077ca0897b1e68ef3ad0d867
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-07-21 18:11:58 -0400

    Removed the multiple planes capability

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/WorldTrace.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/WorldTrace.java
@@ -17,8 +17,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 /**
  * Test JavaDoc generation.
@@ -34,9 +32,9 @@ public class WorldTrace
     private Texture     m_planeTexture;
     private float       m_airportRadius;
 
-    private Sprite             m_earthMap;
-    private LinkedList<Sprite> m_planes;
-    private float              m_planeSize;
+    private Sprite m_earthMap;
+    private Sprite m_plane;
+    private float  m_planeSize;
 
     private ShapeRenderer m_shapeRenderer;
     private Circle        m_arrivalAirport;
@@ -57,7 +55,6 @@ public class WorldTrace
 
         setupBackground();
 
-        m_planes    = new LinkedList<>();
         m_planeSize = 32f;
 
         m_airportRadius    = 3f;
@@ -91,7 +88,7 @@ public class WorldTrace
 
     private void update() {
         updateCamera();
-        translatePlanes();
+        translatePlane();
     }
 
     private void draw() {
@@ -107,21 +104,27 @@ public class WorldTrace
         m_shapeRenderer.setProjectionMatrix(m_camera.combined);
     }
 
-    private void translatePlanes() {
-        for (Iterator<Sprite> iter = m_planes.iterator(); iter.hasNext(); ) {
-            Sprite plane = iter.next();
-
-            if (Intersector.overlaps(m_arrivalAirport, plane.getBoundingRectangle())) {
-                iter.remove();
+    private void translatePlane() {
+        if (m_plane != null) {
+            if (Intersector.overlaps(m_arrivalAirport, m_plane.getBoundingRectangle())) {
+                m_plane = null;
             }
             else {
                 if (Math.abs((m_arrivalAirport.x - m_departureAirport.x)) > Math.abs((m_arrivalAirport.y - m_departureAirport.y))) {
-                    plane.translate((float) Math.abs(100.0 / (m_arrivalAirport.x - m_departureAirport.x)) * (m_arrivalAirport.x - m_departureAirport.x) * Gdx.graphics.getDeltaTime(),
-                                    (float) Math.abs(100.0 / (m_arrivalAirport.x - m_departureAirport.x)) * (m_arrivalAirport.y - m_departureAirport.y) * Gdx.graphics.getDeltaTime());
+                    m_plane.translate((float) Math.abs(100.0 / (m_arrivalAirport.x - m_departureAirport.x))
+                                      * (m_arrivalAirport.x - m_departureAirport.x)
+                                      * Gdx.graphics.getDeltaTime(),
+                                      (float) Math.abs(100.0 / (m_arrivalAirport.x - m_departureAirport.x))
+                                      * (m_arrivalAirport.y - m_departureAirport.y)
+                                      * Gdx.graphics.getDeltaTime());
                 }
                 else {
-                    plane.translate((float) Math.abs(100.0 / (m_arrivalAirport.y - m_departureAirport.y)) * (m_arrivalAirport.x - m_departureAirport.x) * Gdx.graphics.getDeltaTime(),
-                                    (float) Math.abs(100.0 / (m_arrivalAirport.y - m_departureAirport.y)) * (m_arrivalAirport.y - m_departureAirport.y) * Gdx.graphics.getDeltaTime());
+                    m_plane.translate((float) Math.abs(100.0 / (m_arrivalAirport.y - m_departureAirport.y))
+                                      * (m_arrivalAirport.x - m_departureAirport.x)
+                                      * Gdx.graphics.getDeltaTime(),
+                                      (float) Math.abs(100.0 / (m_arrivalAirport.y - m_departureAirport.y))
+                                      * (m_arrivalAirport.y - m_departureAirport.y)
+                                      * Gdx.graphics.getDeltaTime());
                 }
             }
         }
@@ -136,7 +139,7 @@ public class WorldTrace
         m_batch.begin();
 
         drawEarthMap();
-        drawPlanes();
+        drawPlane();
 
         m_batch.end();
     }
@@ -151,9 +154,9 @@ public class WorldTrace
         m_earthMap.draw(m_batch);
     }
 
-    private void drawPlanes() {
-        for (Sprite plane : m_planes) {
-            plane.draw(m_batch);
+    private void drawPlane() {
+        if (m_plane != null) {
+            m_plane.draw(m_batch);
         }
     }
 
@@ -187,11 +190,10 @@ public class WorldTrace
     private void spawnPlane() throws SQLException {
         findCoordinates();
 
-        Sprite plane = new Sprite(m_planeTexture);
-        plane.setSize(m_planeSize, m_planeSize);
-        plane.setCenter(m_departureAirport.x, m_departureAirport.y);
-        plane.flip(m_arrivalAirport.x < m_departureAirport.x, false);
-        m_planes.add(plane);
+        m_plane = new Sprite(m_planeTexture);
+        m_plane.setSize(m_planeSize, m_planeSize);
+        m_plane.setCenter(m_departureAirport.x, m_departureAirport.y);
+        m_plane.flip(m_arrivalAirport.x < m_departureAirport.x, false);
     }
 
     private void findCoordinates() throws SQLException {
