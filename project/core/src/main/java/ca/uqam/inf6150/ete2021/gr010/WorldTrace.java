a845e164187c93628db07a5c151406c516995cdd
Alex <alexandre.bourdeau@mailbox.org>
2021-07-22 19:48:59 -0400

    feature: Added plane flights sequentially

--- project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/WorldTrace.java
+++ project/core/src/main/java/ca/uqam/inf6150/ete2021/gr010/WorldTrace.java
@@ -15,11 +15,12 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
-
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.LinkedList;
 
 /**
- * Main World Trace application.
+ * Main WorldTrace application.
  *
  * <p>
  * Use infographics technologies to render onto the screen plane flights.
@@ -27,6 +28,8 @@ import java.sql.SQLException;
  */
 public class WorldTrace
         extends ApplicationAdapter {
+
+    private final int MIN_LIST_SIZE = 3;
 
     private OrthographicCamera m_camera;
 
@@ -47,6 +50,8 @@ public class WorldTrace
     private Circle  m_departureAirport;
     private Vector2 m_planeDir;
 
+    private LinkedList<Flight> m_flightList;
+
     @Override
     public void create() {
         super.create();
@@ -66,10 +71,11 @@ public class WorldTrace
         m_planeSpeed    = 100f;
         m_airportRadius = 3f;
 
+        m_flightList = new LinkedList<Flight>();
         try {
-            spawnPlane();
-        }
-        catch (SQLException p_ignored) {
+            fetchFlights();
+            spawnFlight();
+        } catch (SQLException p_ignored) {
             // Ignore exceptions
         }
     }
@@ -87,8 +93,12 @@ public class WorldTrace
 
     @Override
     public void render() {
-        update();
-        draw();
+        if (m_flightList.size() == MIN_LIST_SIZE) {
+            fetchFlights();
+        } else {
+            update();
+            draw();
+        }
     }
 
     private void update() {
@@ -98,7 +108,6 @@ public class WorldTrace
 
     private void draw() {
         prepareScreen();
-
         batchDrawSprites();
         batchDrawShapes();
     }
@@ -116,6 +125,13 @@ public class WorldTrace
 
             if (Intersector.overlaps(m_arrivalAirport, m_plane.getBoundingRectangle())) {
                 destroyFlight();
+                try {
+                    if (!m_flightList.isEmpty()) {
+                        spawnFlight();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
             else {
                 Vector2 translation = new Vector2(m_planeDir).scl(m_planeSpeed).scl(Gdx.graphics.getDeltaTime());
@@ -190,11 +206,28 @@ public class WorldTrace
         m_earthMap.setSize(m_camera.viewportWidth, m_camera.viewportHeight);
     }
 
-    private void spawnPlane() throws SQLException {
+    private void fetchFlights() {
+        final int FETCH_SIZE = 5;
+
+        try {
+            if (m_flightList.isEmpty()) {
+                // Timestamp timestamp = new Timestamp(2023, 01, 01, 01, 01, 01, 01);
+                m_flightList.addAll(FlightAPI.fetchLatestSequence(FETCH_SIZE));
+            } else {
+                Timestamp tsFlight = m_flightList.get(MIN_LIST_SIZE - 1).getArrival();
+                // Timestamp tsFetch = new Timestamp(tsFlight.getTime() + 1);
+                m_flightList.addAll(FlightAPI.fetchLatestSequence(tsFlight, FETCH_SIZE));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while fetching the latest flights.");
+        }
+    }
+
+    private void spawnFlight() throws SQLException {
         assert m_arrivalAirport != null;
         assert m_departureAirport != null;
 
-        findCoordinates();
+        findAirportCoordinates();
 
         m_plane = new Sprite(m_planeTexture);
         m_plane.setSize(m_planeSize, m_planeSize);
@@ -202,11 +235,11 @@ public class WorldTrace
         m_plane.flip(m_arrivalAirport.x < m_departureAirport.x, false);
     }
 
-    private void findCoordinates() throws SQLException {
+    private void findAirportCoordinates() throws SQLException {
         assert m_arrivalAirport != null;
         assert m_departureAirport != null;
 
-        Flight flight = FlightAPI.fetchLatest();
+        Flight flight = m_flightList.pop();
 
         Vector2 halfViewport = new Vector2(m_camera.viewportWidth, m_camera.viewportHeight).scl(0.5f);
         City    beginAirport = flight.getBeginAirport().getCity();
