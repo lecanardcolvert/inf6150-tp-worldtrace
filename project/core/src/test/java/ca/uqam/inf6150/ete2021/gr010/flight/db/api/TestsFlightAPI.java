1d33b2368c2bce766070cfbe355bbf421bcc9249
Bensakhria, Rebei Belkacem <bensakhria.rebei_belkacem@courrier.uqam.ca>
2021-07-28 16:15:10 -0400

    Fin des tests

--- project/core/src/test/java/ca/uqam/inf6150/ete2021/gr010/flight/db/api/TestsFlightAPI.java
+++ project/core/src/test/java/ca/uqam/inf6150/ete2021/gr010/flight/db/api/TestsFlightAPI.java
@@ -1,6 +1,7 @@
 package ca.uqam.inf6150.ete2021.gr010.flight.db.api;
 
 import ca.uqam.inf6150.ete2021.gr010.flight.db.api.FlightAPI;
+import ca.uqam.inf6150.ete2021.gr010.flight.gui.math.Degree;
 import lombok.extern.slf4j.XSlf4j;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
@@ -17,7 +18,7 @@ class TestsFlightAPI {
         Timestamp TimeStampDepart = Collections.singletonList(FlightAPI.fetchAll().get(1).getDeparture()).get(0);
         Timestamp testTimeStampDepart = Timestamp.valueOf("2022-01-12 03:30:00.0");
         assertEquals(testTimeStampDepart,TimeStampDepart);
-        log.debug("Date et heure de depart du 2eme vol: "+TimeStampDepart);
+        log.debug("Date et heure de depart du 2eme vol de 2022: "+TimeStampDepart);
     }
 
     //Test de la méthode FlightAPI.fetchAll()
@@ -64,7 +65,7 @@ class TestsFlightAPI {
     public void testCoordonnesArriveeLat() throws SQLException {
         double testCoorArr = FlightAPI.fetchAll().get(27).getEndAirport().getCity().getLatitude();
         assertEquals(-4.2634,testCoorArr);
-        log.debug("Latitude ville d'arrivee: "+testCoorArr);
+        log.debug("Latitude de ville d'arrivee du vol 28: "+testCoorArr);
     }
 
     //Test de la méthode FlightAPI.fetchAll()
@@ -72,7 +73,7 @@ class TestsFlightAPI {
     public void testCoordonnesDepLong() throws SQLException {
         double testCoorDep= FlightAPI.fetchAll().get(14).getBeginAirport().getCity().getLongitude();
         assertEquals(-75.6972,testCoorDep);
-        log.debug("Longitude ville de depart: "+testCoorDep);
+        log.debug("Longitude ville de depart du vol 15: "+testCoorDep);
     }
 
     //Test de la méthode FlightAPI.fetchForemostInTime()
@@ -81,7 +82,7 @@ class TestsFlightAPI {
         Timestamp TimestampDepart = FlightAPI.fetchForemostInTime().getDeparture();
         Timestamp testTimeStampDepart = Timestamp.valueOf("2022-02-10 21:05:00.0");
         assertEquals(testTimeStampDepart,TimestampDepart);
-        log.debug("Dernier vol: "+TimestampDepart);
+        log.debug("Dernier vol prevu: "+TimestampDepart);
 
     }
 
@@ -100,6 +101,54 @@ class TestsFlightAPI {
         Timestamp testTimestampLatest = Timestamp.valueOf("2021-07-19 23:55:00.0");
         assertEquals(testTimestampLatest,TimestamplLatest);
         log.debug("Vol le plus proche de notre date: " + TimestamplLatest.toString());
+    }
+
+    //Test de la méthode FlightAPI.fetchLatest(Timestamp)
+    @Test
+    public void testLatestDepartAvecDate() throws SQLException {
+        Timestamp TimestampLatest = Timestamp.valueOf("2021-10-07 03:45:00.0");
+        Timestamp testTimestampLatest = Timestamp.valueOf("2021-10-10 01:44:00.0");
+        assertEquals(TimestampLatest,FlightAPI.fetchLatest(testTimestampLatest).getDeparture());
+        log.debug("Dernier vol avant le 10 octobre 2020 a 1h44: " + TimestampLatest);
+    }
+
+    //Test de la méthode FlightAPI.fetchLatest(Timestamp)
+    @Test
+    public void testLatestDepartAvecDate2() throws SQLException {
+        Timestamp TimestampLatest = Timestamp.valueOf("2021-09-02 10:05:00.0");
+        Timestamp testTimestampLatest = Timestamp.valueOf("2021-08-27 02:19:00.0");
+        assertNotEquals(TimestampLatest,FlightAPI.fetchLatest(testTimestampLatest).getDeparture());
+        log.debug("Dernier vol avant le 27 aout 2021 a 2h19: " +
+                FlightAPI.fetchLatest(testTimestampLatest).getDeparture());
+    }
+
+    //Test de la méthode FlightAPI.fetchLatestSequence(int)
+    @Test
+    public void testLatestSequenceInt() throws SQLException {
+        String villeDepart = FlightAPI.fetchLatestSequence(2).
+                get(1).getBeginAirport().getCity().getName();
+        assertEquals("Rome",villeDepart);
+        log.debug("Ville de depart du vol avant celui le plus proche d'aujourd'hui: "+villeDepart);
+    }
+
+    //Test de la méthode FlightAPI.fetchLatestSequence(Timestamp,int)
+    @Test
+    public void testLatestSequenceTimestamp() throws SQLException {
+        Timestamp TimestampLatest = Timestamp.valueOf("2021-09-02 10:05:00.0");
+        String compagnieAerienne = FlightAPI.fetchLatestSequence(TimestampLatest,3).
+                get(0).getAirline().getName();
+        assertEquals("Hong Kong Airlines Limited",compagnieAerienne);
+        log.debug("Compagnie aerienne du vol le plus proche du 2 septembre 2021: "+ compagnieAerienne);
+    }
+
+    //Test de la méthode FlightAPI.fetchLatestSequence(Timestamp,int)
+    @Test
+    public void testLatestSequenceTimestamp2() throws SQLException {
+        Timestamp TimestampLatest = Timestamp.valueOf("2021-11-11 04:20:00.0");
+        String model = FlightAPI.fetchLatestSequence(TimestampLatest,4).
+                get(2).getAircraft().getModel();
+        assertEquals("717",model);
+        log.debug("Model d'avion du 3eme vol avant celui du 11 novembre 2021: "+model);
     }
 
 
